e37d3900ec7bfc7b8571783f1ef315308b720efa
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2023-06-17 01:20:55 +0600

    Adding FrontEnd

--- Project01/BackEnd/app/apis/api.py
+++ Project01/BackEnd/app/apis/api.py
@@ -2,7 +2,7 @@ from pkgutil import get_data
 from app import app
 from flask import request, jsonify
 import json
-from app.apis.crud import deleteAllDataFromRedis, getRedisCache, inserting, getAllData, setRedisCache
+from app.apis.crud import deleteAllDataFromRedis, deleting, getDataById, getRedisCache, inserting, getAllData, setRedisCache, updating
 
 # by defaul it will be GET method
 @app.route("/")
@@ -22,30 +22,84 @@ def insertDataToDB():
     else:
         return jsonify({"Please provide all the required fields"}), 400
     
-    # students = getAllData()
-    # serialized_students = [student.serialize() for student in students]
-    # return jsonify({'message': 'success', 'data': serialized_students}), 200
-
-@app.route("/getall", methods=["GET"])
+    
+@app.route("/getlist", methods=["GET"])
 def getDataFromDB():
-    try:
-        res = getRedisCache()
-        if res:
-            return jsonify({'message': 'success', 'isCached': 'yes', 'data': res}), 200
-
         students = getAllData()
         serialized_students = [student.serialize() for student in students]
         data = []
         for student in serialized_students:
-            data.append({'name': student['name'], 'phone': student['phone'], 'numberofexp': student['numberofexp']})
-
-        setRedisCache(json.dumps(serialized_students))
+            data.append({'id': student['id'], 'name': student['name'], 'phone': student['phone'], 'numberofexp': student['numberofexp']})
         return jsonify({'message': 'success', 'isCached': 'No', 'data': data}), 200
 
-    except Exception as e:
-        return jsonify({'message': 'error', 'error': str(e)}), 500
+@app.route("/delete", methods=["DELETE"])
+def deleteDataFromDB():
+    payload = request.get_json()
+    id = payload["id"]
+    
+    if id:
+        res = deleting(id)
+        if res:
+            return jsonify({"student deletion successfully": res.serialize()}), 201
+        else:
+            return jsonify({"Cannot find any student with the id": id}), 400
+
+    else:
+        return jsonify({"Please Provide an ID to delete"}), 400
 
 
+
+@app.route("/update", methods=["PUT"])
+def updateDatafromDB():
+    payload = request.get_json()
+    id = payload["id"]
+    name = payload["name"]
+    if id and name:
+        res = updating(id, name)
+        if res:
+            return jsonify({"student update successfully": res.serialize()}), 201
+        else:
+            return jsonify({"Cannot find any student with the id": id}), 400
+
+    else:
+        return jsonify({"Please Provide an ID and Name to udpate"}), 400
+
+
+
+@app.route("/getbyid", methods=["GET"])
+def getSingleDataFromDB():
+        payload = request.get_json()
+        id = payload["id"]
+        if id:
+
+            res = getDataById(id)
+            if res:
+                return jsonify({"student": res.serialize()}), 201
+            else:
+                return jsonify({"Cannot find any student with the id": id}), 400
+        else:
+            return jsonify({"Please Provide an ID to get"}), 400
+
+# @app.route("/getall", methods=["GET"])
+# def getDataFromDB():
+#     try:
+#         res = getRedisCache()
+#         if res:
+#             return jsonify({'message': 'success', 'isCached': 'yes', 'data': res}), 200
+
+#         students = getAllData()
+#         serialized_students = [student.serialize() for student in students]
+#         data = []
+#         for student in serialized_students:
+#             data.append({'name': student['name'], 'phone': student['phone'], 'numberofexp': student['numberofexp']})
+
+#         setRedisCache(json.dumps(serialized_students))
+#         return jsonify({'message': 'success', 'isCached': 'No', 'data': data}), 200
+
+#     except Exception as e:
+#         return jsonify({'message': 'error', 'error': str(e)}), 500
+    
+  
 
 # @app.route("/deleteallredis", methods=["DELETE"])
 # def getDataFromDB():
--- Project01/BackEnd/app/apis/crud.py
+++ Project01/BackEnd/app/apis/crud.py
@@ -8,8 +8,28 @@ def inserting(name, phone, numberofexp):
     db.session.commit()
     return variable.id
 
+def deleting(id):
+    student = Students.query.get(id)
+    if student:
+        db.session.delete(student)
+        db.session.commit()
+        return student
+    else:
+        return False
+    
+def updating(id, name):
+    student = Students.query.get(id)
+    if student:
+        student.name = name
+        db.session.commit()
+        return Students.query.get(id)
+    else:
+        return False
+
+
 def getAllData():
     return Students.query.all()
+
 def getDataById(id):
     return Students.query.filter_by(id=id).first()
 
@@ -18,11 +38,13 @@ def setRedisCache(name, phone, numberofexp):
     redis_db.set("phone", phone)
     redis_db.set("numberofexp", numberofexp)
     return
+
 def getRedisCache():
     name = redis_db.get("name")
     phone = redis_db.get("phone")
     numberofexp = redis_db.get("numberofexp")
     return {"name": name, "phone": phone, "numberofexp": numberofexp}
+
 def deleteRedisCache():
     redis_db.delete("name")
     redis_db.delete("phone")
